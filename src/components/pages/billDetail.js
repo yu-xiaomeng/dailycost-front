6ddcd0e2ff683ef8c9770b96fef7c480798d9580
yu-xiaomeng <414017727@qq.com>
2022-01-05 15:34:57 +0800

    delete one bill and redirect to billList page

--- src/components/pages/billDetail.js
+++ src/components/pages/billDetail.js
@@ -2,13 +2,15 @@ import React from "react";
 import axios from "axios";
 import cookie from "react-cookies";
 import { Descriptions } from 'antd';
-import { NavBar, Button,  Modal, Toast} from 'antd-mobile'
+import { NavBar, Button,  Modal, Toast} from 'antd-mobile';
+import { Navigate } from "react-router-dom";
 
 class BillDetail extends React.Component {
     state = {
         token: cookie.load("token"),
         id: "",
         bill: {},
+        deleted: false,
     }
 
     getBillDetailById(id) {
@@ -16,6 +18,18 @@ class BillDetail extends React.Component {
             headers: {"Authorization": 'Bearer '+this.state.token}
         }).then(res => {
             this.setState({bill: res.data.data});
+        })
+    }
+
+    deleteBillById(id) {
+        axios.delete('/api/bill/'+id,{
+            headers: {"Authorization": 'Bearer '+this.state.token}
+        }).then(res => {
+            if(res.status === 200){
+                this.setState({deleted: true});
+                console.log(this.state.deleted);
+            }
+            
         })
     }
 
@@ -34,6 +48,9 @@ class BillDetail extends React.Component {
     }
 
     render () {
+        if (this.state.deleted === true) {
+            return (<Navigate to="/billList" />)
+        }
         return (
             <div>
                 <NavBar onBack={this.back}>
@@ -50,7 +67,7 @@ class BillDetail extends React.Component {
                     <Descriptions.Item label="备注">{this.state.bill.note}</Descriptions.Item>
                     
                 </Descriptions>
-                <Button>编辑</Button>
+                {/* <Button>编辑</Button> */}
                 <Button
                     onClick={() =>
                     Modal.confirm({
@@ -58,12 +75,12 @@ class BillDetail extends React.Component {
                         content: '删除后数据不可恢复',
                         // confirmText: "删除",
                         onConfirm: async () => {
-                            // await getBillDetailById();
+                            this.deleteBillById(this.state.id);
                         // await sleep(3000)
                         Toast.show({
                             icon: 'success',
-                            content: '提交成功',
-                            position: 'bottom',
+                            content: '删除成功',
+                            position: 'center',
                         })
                         },
                     })
