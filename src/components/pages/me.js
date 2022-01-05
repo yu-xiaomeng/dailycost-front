90427ec3851319bc488c3b6862e2b34c9b147912
yu-xiaomeng <414017727@qq.com>
2022-01-05 21:40:34 +0800

    add me page

--- src/components/pages/me.js
+++ src/components/pages/me.js
@@ -1,13 +1,45 @@
+import axios from 'axios';
 import React from 'react'
+import cookie from 'react-cookies';
 import BottomBar from "../tabs/bottomBar";
+
+import {Image} from 'antd-mobile';
 
 
 class Me extends React.Component {
+    state = {
+        token: cookie.load("token"),
+        userInfo: {},
+
+    }
+
+    componentDidMount() {
+        this.getUserInfo();
+    }
+
+    getUserInfo() {
+        axios.get('/api/user', {
+            headers: {"Authorization": 'Bearer '+this.state.token}
+        }).then(res => {
+            console.log(res.data.data);
+            this.setState({userInfo: res.data.data});
+        })
+    }
     render () {
         return (
             <div className="aaa">
                 <div className="pagecontent">
-                    <div>mingxi</div>
+                    <div>
+                        {/* {this.state.userInfo.avatar} */}
+                        <Image
+                            src={this.state.userInfo.avatar}
+                            width={64}
+                            height={64}
+                            fit='cover'
+                            style={{ borderRadius: 32 }}
+                        />
+                        yuxiaomeng
+                    </div>
                 </div>
                 <div className="tab-bar">
                 <BottomBar />
